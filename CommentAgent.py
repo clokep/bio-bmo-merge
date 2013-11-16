5c723d47f83690972c26ded0893fef46efd4f165
Patrick Cloke <clokep@gmail.com>
2013-11-16 16:13:16 -0500

    Add the rest of the JS stuff into the python stuff.

---
+++ CommentAgent.py
@@ -0,0 +1,22 @@
+from bugzilla.models import Bug, Attachment
+from scripts.attach import AttachmentAgent
+
+class CommentAgent(AttachmentAgent):
+    def create_bug(self, bug):
+        """Create a new bug."""
+
+        fields = {
+            'product': bug.product,
+            'component': bug.componen,
+            'summary': bug.summary,
+            'version': bug.version,
+            'op_sys': bug.op_sys,
+            'platform': bug.platform,
+            'priority': bug.priority,
+        }
+
+        url = urljoin(self.API_ROOT, 'bug?%s' % self.qs())
+        return Bug(**fields).post_to(url)
+
+    def add_comment(self, bug_id, comment):
+        self._comment(bug_id, comment)
--- bugzilla-merge.py
+++ bugzilla-merge.py
@@ -4,11 +4,45 @@ from scripts.attach import AttachmentAgent
 from os import mkdir, path
 import shutil
 import urllib
+from CommentAgent import CommentAgent
+
+# See https://bmo.etherpad.mozilla.org/bugzilla-instantbird-merge for many of
+# the details about how this script was developed.
 
 # Transform the fields from BIO to BMO, including:
-#   Product
-#   Component
+#   product
+#   component
+#   platform
+#   op_sys
 #
+# This will be run once on each bug.
+def mapFields(bug):
+    # Addons/* -> Client Software/Instantbird/Demo Add-ons
+    if (bug.product == "Add-ons"):
+        bug.product = "Instantbird"
+        bug.component = "Demo Add-ons"
+    # Core/* -> Components/Chat Core/*
+    elif (bug.product == "Core"):
+        bug.product = "Chat Core"
+    # Instantbird/* -> Client Software/Instantbird/*
+    elif (bug.product == "Instantbird (UI)"):
+        bug.product = "Instantbird"
+    # Websites/* -> Other/Instantbird Servers/*
+    elif (bug.product == "Websites"):
+        bug.product = "Instantbird Servers"
+    else:
+        raise "Unable to map product: %s" % bug.product;
+
+    # BIO separate PC and Mac, BMO has both as x86.
+    if (bug.platform == "PC" or bug.platform == "Mac"):
+        bug.platform = "x86"
+
+    # This will knowingly cause some dataloss, but it should be minimal.
+    if (bug.op_sys == "Windows (all)"):
+        bug.op_sys = "Windows XP"
+    if (bug.op_sys.startswith("Mac OS")):
+        bug.op_sys = "Mac OS X"
+
 # Also modify some fields to properly reference the new bug and attachment
 # numbers:
 #   Comment
@@ -20,8 +54,16 @@ import urllib
 #   Creator
 #   Commentor
 #   CC list
-def transform(bug):
-    return bug
+
+# Create mapping data structures.
+bugIdMap = {}
+attachmentIdMap = {}
+
+def mapIds(text):
+    #bugPattern = /(bug\s#?|show_bug\.cgi\?id=)(\d+)/gi;
+    #attachmentPattern = /(attachment\s#?)(\d+)/gi;
+    #sourceUrlPattern = /bugzilla\.instantbird\.(?:org|com)/gi;
+    return text
 
 # We can use "None" for both instead to not authenticate
 username, password = get_credentials()
@@ -33,38 +75,57 @@ username, password = get_credentials()
 # Load our agent for BMO
 bio = BugzillaAgent("https://api-dev.bugzilla.mozilla.org/instantbird/",
                     username, password)
-bioa = AttachmentAgent(bio)
 bioUrl = "https://bugzilla.instantbird.org"
 #bmo = BMOAgent(username, password)
-bmo = BugzillaAgent("https://api-dev.bugzilla.mozilla.org/test/latest",
-                    username, password)
-bmoa = AttachmentAgent(bmo)
+bmo = CommentAgent("https://api-dev.bugzilla.mozilla.org/test/latest/",
+                   username, password)
 
 # Set whatever REST API options we want
 options = {
-    'changed_after':    '2013-11-12',
+    'changed_after':    '2013-01-12',
     #'include_fields':   '_default,attachments',
     'include_fields':   '_all',
 }
 
 # Get the bugs from the source.
-buglist = bio.get_bug_list(options)
+bugs = bmo.get_bug_list(options)
 
-print "Found %s bugs" % (len(buglist))
+print "Found %s bugs" % (len(bugs))
 
 # Create the directory for attachments.
 attachmentsDir = "attachments"
+print "Removing '%s' directory" % attachmentsDir
 shutil.rmtree(attachmentsDir)
+print "Creating '%s' directory" % attachmentsDir
 mkdir(attachmentsDir)
 
+# Sort the bugs by ID.
+print "Sorting bugs"
+bugs.sort(key=lambda bug: bug.id)
+
 # Download all attachments.
-for bug in buglist:
-    print bug
+print "Downloading attachments..."
+for bug in bugs:
+    #print u"Processing bug %d - %s" % (bug.id, bug.summary)
+    print "Processing bug %s" % bug.id
     for attachment in bug.attachments:
-        print attachment
+        #print "Downloading attachment %d - %s" %(attachment.id, attachment.description)
+        print "Downloading attachment %d" % attachment.id
         url = bioUrl + "/attachment.cgi?id=" + str(attachment.id)
         urllib.urlretrieve(url, path.join(attachmentsDir, str(attachment.id)))
 
 # Create new bugs from the first comment + add an attachment if there is one.
+for bug in bugs:
+    # First update the fields.
+    mapFields(bug)
+    bug.summary = mapIds(bug.summary)
+    #bmo.create_bug(bug)
 
-# Now add all other comments / attachments in order.
+# Sort the comments / attachments by date.
+comments = [comment for bug in bugs for comment in bug.comments]
+comments.sort(key=lambda comment: comment.creation_time)
+
+print len(comments)
+# Now add all other comments / attachments.
+
+# Now add dependencies.
